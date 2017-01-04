7a75a8f324bf15a9e5e65d74d0c85337faaedbef
Bruno Paulino <brunojppb@gmail.com>
2017-01-04 11:54:09 +0100

    Circular animation view for traking load progress

---
+++ circularLoadingAnimation.playground/Contents.swift
@@ -0,0 +1,99 @@
+import UIKit
+import XCPlayground
+import PlaygroundSupport
+
+class LoadingCircleView: UIView {
+    
+    private var circleAnimationLayer = CAShapeLayer()
+    fileprivate var currentProgress: CGFloat = 0.0
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.frame.size.width/2
+        self.addAnimationLayer()
+    }
+    
+    private func addAnimationLayer() {
+        self.circleAnimationLayer.removeFromSuperlayer()
+        let angle: CGFloat = 270 * (.pi / 180.0)
+        let circlePath = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2), radius: self.frame.size.width/4, startAngle: angle, endAngle: 630 * (.pi / 180.0), clockwise: true)
+        self.circleAnimationLayer.path = circlePath.cgPath
+        self.circleAnimationLayer.fillColor = UIColor.clear.cgColor
+        self.circleAnimationLayer.strokeColor = UIColor.white.cgColor
+        self.circleAnimationLayer.lineWidth = self.frame.size.width/2
+        self.circleAnimationLayer.strokeEnd = 0
+        
+        self.layer.addSublayer(self.circleAnimationLayer)
+    }
+    
+    func animate(with progress: Double) {
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.duration = 2
+        animation.fromValue = currentProgress
+        animation.toValue = progress
+        animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionLinear)
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = kCAFillModeForwards
+        animation.delegate = self
+        self.circleAnimationLayer.strokeEnd = 1
+        self.circleAnimationLayer.add(animation, forKey: "animateCircle")
+        self.currentProgress = CGFloat(progress)
+    }
+}
+
+extension LoadingCircleView: CAAnimationDelegate {
+    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        if self.currentProgress >= 1 {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.alpha = 0.0
+            })
+        }
+    }
+}
+
+let containerView = UIView(frame: CGRect(x: 0, y: 0, width: 370, height: 370))
+containerView.backgroundColor = UIColor.brown
+let circleSize: CGFloat = 30
+PlaygroundPage.current.liveView = containerView
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let circleView = LoadingCircleView()
+containerView.addSubview(circleView)
+circleView.layer.borderColor = UIColor.white.cgColor
+circleView.layer.borderWidth = 2
+circleView.translatesAutoresizingMaskIntoConstraints = false
+
+var constraints = [NSLayoutConstraint]()
+constraints.append(NSLayoutConstraint(item: circleView,
+                                      attribute: .left,
+                                      relatedBy: .equal,
+                                      toItem: containerView,
+                                      attribute: .left,
+                                      multiplier: 1,
+                                      constant: 10))
+constraints.append(NSLayoutConstraint(item: circleView,
+                                      attribute: .bottom,
+                                      relatedBy: .equal,
+                                      toItem: containerView,
+                                      attribute: .bottom,
+                                      multiplier: 1,
+                                      constant: -10))
+constraints.append(NSLayoutConstraint(item: circleView,
+                                      attribute: .width,
+                                      relatedBy: .equal,
+                                      toItem: nil,
+                                      attribute: .notAnAttribute,
+                                      multiplier: 1,
+                                      constant: circleSize))
+constraints.append(NSLayoutConstraint(item: circleView,
+                                      attribute: .height,
+                                      relatedBy: .equal,
+                                      toItem: nil,
+                                      attribute: .notAnAttribute,
+                                      multiplier: 1,
+                                      constant: circleSize))
+
+NSLayoutConstraint.activate(constraints)
+
+circleView.animate(with: 1)
+
